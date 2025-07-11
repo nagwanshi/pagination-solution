381718381e867c4bd81922002a867e757809e04e
sapna <nagwanshisapna@gmail.com>
2025-07-11 11:39:06 +0530

    Fetch product data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <div>Hello</div>;
+  const [products, setProducts] = useState([]);
+  const fetchProductAsync = async () => {
+    const res = await fetch("https://dummyjson.com/products?limit=100");
+    const data = await res.json();
+
+    if (data && data.products) {
+      setProducts(data.products);
+    }
+  };
+
+  useEffect(function () {
+    fetchProductAsync();
+  }, []);
+
+  return <div></div>;
 }
 
 export default App;

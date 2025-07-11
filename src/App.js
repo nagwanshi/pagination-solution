63aa239610b9f0ba423b82fdd5f66f21f5946b91
sapna <nagwanshisapna@gmail.com>
2025-07-11 11:39:23 +0530

    Display products

--- src/App.js
+++ src/App.js
@@ -15,7 +15,22 @@ function App() {
     fetchProductAsync();
   }, []);
 
-  return <div></div>;
+  return (
+    <div>
+      {products.length > 0 && (
+        <div className="products">
+          {products.map((prod) => {
+            return (
+              <span className="products__single">
+                <img src={prod.thumbnail} alt={prod.title} />
+                <span>prod.title</span>
+              </span>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
